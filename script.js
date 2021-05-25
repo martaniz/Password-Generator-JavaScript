6b2761da039f2ce4a98441b1b1ed25b285e28eaa
MsNiz <mniz416@gmail.com>
2021-05-24 22:47:25 -0400

    made the length of characgters work

--- script.js
+++ script.js
@@ -1,88 +1,33 @@
-// Assignment code here
+// Assignment Code
 
-
-//start with var
-
-var useNumbers;
-var useSpeacialCharacters;
-var useUpperCase;
-var useLowerCase;
-
-
-
-//password characters
-
-specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "<", "=", ">", "?", "@", "[", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-
-lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
-"O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-var passwordVar=[]
-
-
-
-// function length 
-
-
-
-
-
-// FUNCTION generatePassword
-function generatePassword() {
-
-//prompt user imput password length 
-
-var length =  parseInt(prompt("Chose a password length that is at least 8 characters and no more then 128"))
-  console.log(length)
-
-  if (length <= 8 || length >= 999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999 || length == NaN || length === "" || length === null ) {
-    confirm("Make sure the password is between 8 and 128 characters long ")
-
-  } 
-  
-  //use numbers/specialCharacters/upper and lower
-  else {
-    useNumbers = confirm("Would you like the password to include numbers");
-    useSpeacialCharacters = confirm("Would you like the password to include specail characteres");
-    useLowerCase = confirm("Would you like the password to include lower case ");
-    useUpperCase = confirm("Would you like the password to include upper case");
-  }
-
-  if (!useNumbers && !useSpeacialCharacters && !useLowerCase && !useUpperCase) {
-    choices = alert("You must choose a criteria!");
-console.log(length)
-}
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-// Get references to the #generate element
+//lets write out our varibles 
 var generateBtn = document.querySelector("#generate");
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChar = "0123456789";
+var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|"
+var passwordLength;
+var uppercaseCheck;
+var numberCheck;
+var specialCheck;
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//first we need to determin the lenght of the password. I tried may different way to make the character lenght work and pysicaly typing in the length of the characters some how worked
 
-  passwordText.value = password;
 
+function determineLength(){
+  passwordLength = prompt("Choose how many characters long you'd like your password to be (between 8-128 characters): ");
+
+    if (passwordLength<00000008){
+      alert("Password length must be a number between 8-128 characters");
+      determineLength();
+    }else if (passwordLength>99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999){
+      alert("Password length must be a number between 8-128 characters");
+      determineLength();
+    }else if (isNaN(passwordLength)){
+      alert("Password length must be a number between 8-128 characters");
+      determineLength();
+    }else{
+    alert("The next three screens will ask you what types of characters you would like to be included in your password.\nIf you choose 'No' for all, your password will only contain lowercase letters.");
+    }
+    return passwordLength;
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
